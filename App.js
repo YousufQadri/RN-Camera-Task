70b03e894d1f4cc2c656acc392d9a9d2f55263bb
Yousuf <s.yousuf@live.com>
2019-09-07 21:46:41 +0500

    Camera functioning properly

--- App.js
+++ App.js
@@ -1,19 +1,125 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import * as Permissions from "expo-permissions";
+import { Camera } from "expo-camera";
+import CamView from "./components/CamView";
+import NoCamAccess from "./components/NoCamAccess";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    cameraPermission: null,
+    type: Camera.Constants.Type.back,
+    isCamera: false
+  };
+
+  openCamera = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    if (status === "granted") {
+      this.setState({ cameraPermission: true, isCamera: true }, () =>
+        console.log("Camera open time state", this.state)
+      );
+    } else {
+      this.setState({ cameraPermission: false, isCamera: false });
+    }
+  };
+
+  getSnap = async () => {
+    const photo = await this.camera.Permissions.takePictureAsync();
+    this.setState({ photo: photo.uri, isCamera: false }, () =>
+      console.log("Snap time state", this.state)
+    );
+  };
+
+  render() {
+    console.log("Render time state", this.state);
+
+    const { cameraPermission, isCamera } = this.state;
+    return (
+      <View style={{ flex: 1 }}>
+        {!isCamera && (
+          <View style={styles.container}>
+            <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+              Welcome to Camera App
+            </Text>
+            <TouchableOpacity style={styles.button} onPress={this.openCamera}>
+              <Text>Open Camera</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+
+        {!!this.state.isCamera && (
+          <Camera
+            style={{ flex: 1 }}
+            type={this.state.type}
+            ref={ref => {
+              this.camera = ref;
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: "transparent",
+                flexDirection: "row"
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  flex: 0.5,
+                  alignSelf: "flex-end",
+                  alignItems: "center"
+                }}
+                onPress={() => {
+                  this.setState({
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                  });
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                >
+                  {" "}
+                  Flip{" "}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={this.snap}
+                style={{
+                  flex: 0.5,
+                  alignSelf: "flex-end",
+                  alignItems: "center"
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                >
+                  Capture
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "skyblue",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  button: {
+    padding: 6,
+    margin: 10,
+    color: "white",
+    fontSize: 18,
+    alignItems: "center",
+    backgroundColor: "#e74c3c",
+    borderRadius: 5
+  }
 });
